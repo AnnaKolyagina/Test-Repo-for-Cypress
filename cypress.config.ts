fccf9ecd63e973c76d2903ea46a5b798dc3043da
Anna Kolyagina <kolyagina.ann@gmail.com>
2026-01-31 14:00:14 +0100

    Config for Circle CI upd4

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,12 +4,6 @@ export default defineConfig({
   video: true,
   screenshotOnRunFailure: true,
 
-  reporter: "junit",
-  reporterOptions: {
-    mochaFile: "cypress/results/junit-[hash].xml",
-    toConsole: true,
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
       //сюда можно добавлять плагины, события
